fec477914adce5e84cbd029ca76a08cfc7f21279
Grzegorz Stuczynski <stuczynski.g@gmail.com>
2019-02-09 20:38:48 +0100

    first commit

---
+++ client.js
@@ -0,0 +1,18 @@
+import {ReactInstance} from 'react-360-web';
+
+function init(bundle, parent, options = {}) {
+  const r360 = new ReactInstance(bundle, parent, {
+    fullScreen: true,
+    ...options,
+  });
+
+  r360.renderToSurface(
+    r360.createRoot(),
+    r360.getDefaultSurface()
+  );
+  imgFile = new URLSearchParams(window.location.search).get('img')
+
+  r360.compositor.setBackground(r360.getAssetURL(imgFile));
+}
+
+window.React360 = {init};
